0a9d67e6827d1dc460cd26a848b7429e9e438e44
Anthony-Scorgie <149743023+Anthony-Scorgie@users.noreply.github.com>
2023-11-03 12:48:18 +1100

    Create OlympicOrdering.py

---
+++ OlympicOrdering.py
@@ -0,0 +1,10 @@
+sen = input("Country: ")
+con = []
+while sen != '':
+  con.append(sen)
+  sen = input("Country: ")
+print('Greece')
+for i in con:
+  print(i)
+print("Refugee Olympic Team")
+print("Japan")
