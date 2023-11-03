644f1f7c6dbfb097b5c33eb699f5fb38cea678b9
Anthony-Scorgie <149743023+Anthony-Scorgie@users.noreply.github.com>
2023-11-03 12:24:51 +1100

    Create  DescritiveDesserts.py

---
+++ DescritiveDesserts.py
@@ -0,0 +1,9 @@
+bak = input("Treat? ")
+if bak.lower() == 'pastry':
+  print('Buttery flaky goodness')
+elif bak.lower() == 'cake':
+  print('A light and fluffy classic')
+elif bak.lower() == 'cookie':
+  print('Warm and crispy on the edges')
+else:
+  print('We do not sell that. It sounds yum!')
